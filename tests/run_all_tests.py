42250d57e64acb3e7da22aa0b6c3254978022ffc
Tanran <taz609@g.harvard.edu>
2022-12-03 01:18:19 -0500

    UPDATE: run_all_tests.py

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -1,5 +1,5 @@
 import os
-
+from tqdm import tqdm
 # NOTE: modify this if tests cases are stored in path different that pwd
 TEST_FOLDER = "./tests"
 
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     numOfTest = [f[4:-4] for f in os.listdir(TEST_FOLDER) if f.startswith('test') and f.endswith('.txt')]
 
     num_of_correct = 0
-    for test in numOfTest:
+    for test in tqdm(numOfTest, desc='Running tests '):
         testFile = os.path.join("tests", f"test{str(test)}.txt")
         execution = f"python3 {os.path.join('src', 'main.py')} --testFile {testFile}"
         cmd = os.system(execution)
@@ -24,6 +24,9 @@ if __name__ == "__main__":
             correctOut = os.path.join("tests", "correct_output", f"test{str(test)}.out")
             if not os.path.exists(outFile):
                 print(f"{outFile} is not exist!!!")
+                continue
+            if not os.path.exists(correctOut):
+                print(f"Correct output {correctOut} is not exist!!!")
                 continue
             if not cmp_file(outFile, correctOut):
                 print(f"{outFile} is not correct!!!")
